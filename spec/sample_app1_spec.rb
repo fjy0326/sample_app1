91de645404e2e51d09c5602ef717b46bc15d01ad
fjy0326 <bungakukai2010@gmail.com>
2025-01-18 13:27:00 +0000

    20250118update

--- spec/sample_app1_spec.rb
+++ spec/sample_app1_spec.rb
@@ -3,3 +3,9 @@ reauire 'rails_helper'
 RSpec.describe List, "モデルに関するテスト", type: :model do
 
 
+FactoryBot.define do
+  factory :list do
+    title { Faker::Lorem.characters(number:5) }
+    body { Faker::Lorem.characters(number:20) }
+  end
+end
