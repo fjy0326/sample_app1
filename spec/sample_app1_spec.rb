a9d4e0c593a0ae9bed40ba0bb27a179476e2dafe
fjy0326 <bungakukai2010@gmail.com>
2025-01-22 12:25:44 +0000

    20250122update

--- spec/sample_app1_spec.rb
+++ spec/sample_app1_spec.rb
@@ -33,18 +33,31 @@ describe '投稿のテスト' do
   end
 
   describe '投稿画面のテスト' do
-    before ：投稿画面への遷移
-    context：表示の確認 
-      it ：new_list_pathが"/lists/new"であるか'
-        テストコード
-      it：投稿ボタンが表示されているか'
-        テストコード
-    context：投稿処理のテスト
-      it：投稿後のリダイレクト先は正しいか
-        テストコード
+    before do
+      visit new_list_path
+    end
+    context '表示の確認' do 
+      it 'new_list_pathが"/lists/new"であるか' do
+         expect(current_path).to eq('/lists/new')
+      end
+      it '投稿ボタンが表示されているか' do
+         expect(page).to have_buttom '投稿'
+      end
+    end
+    context　'投稿処理のテスト' do
+      it '投稿後のリダイレクト先は正しいか' do
+         fill_in 'list[title]',with: Faker::Lorem.characters(number:10)
+         fill_in 'list[body]',with: Faker::Lorem.characters(number:30)
+         click_buttom '投稿'
+         expect(page).to have_current_path list_path(List.last)
+      end
+    end
+  end  
 
-  describe：一覧画面のテスト
-    before：一覧画面への遷移
+  describe '一覧画面のテスト' do
+    before do
+      visit lists_path
+    end
     context ：一覧の表示とリンクの確認
       it：一覧表示画面に投稿されたものが表示されているか
         テストコード
