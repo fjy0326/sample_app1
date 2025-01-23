398bcea9c69f1f113291802d7aa1014cf2cbd74e
fjy0326 <bungakukai2010@gmail.com>
2025-01-23 03:25:35 +0000

    20250123update

--- spec/sample_app1_spec.rb
+++ spec/sample_app1_spec.rb
@@ -1,21 +1,6 @@
 # frozen_string_literal: true
-reauire 'rails_helper'
+require 'rails_helper'
 
-RSpec.describe List, "モデルに関するテスト", type: :model do
-
-
-FactoryBot.define do
-  factory :list do
-    title { Faker::Lorem.characters(number:10) }
-    body { Faker::Lorem.characters(number:30) }
-  end
-  
-  
-  describe 'モデルのテスト' do
-    it "有効な投稿内容の場合は保存されるか" do
-      expect(FctoryBot.build(:list)).to be_valid
-  　end
-  　
 describe '投稿のテスト' do
   let!(:list) { create(:list,title:'hoge',body:'body') }
   describe 'トップ画面(top_path)のテスト' do
@@ -31,7 +16,7 @@ describe '投稿のテスト' do
       end
     end
   end
-
+  
   describe '投稿画面のテスト' do
     before do
       visit new_list_path
@@ -44,7 +29,7 @@ describe '投稿のテスト' do
          expect(page).to have_buttom '投稿'
       end
     end
-    context　'投稿処理のテスト' do
+    context '投稿処理のテスト' do
       it '投稿後のリダイレクト先は正しいか' do
          fill_in 'list[title]',with: Faker::Lorem.characters(number:10)
          fill_in 'list[body]',with: Faker::Lorem.characters(number:30)
@@ -58,38 +43,62 @@ describe '投稿のテスト' do
     before do
       visit lists_path
     end
-    context ：一覧の表示とリンクの確認
-      it：一覧表示画面に投稿されたものが表示されているか
-        テストコード
+    context '一覧の表示とリンクの確認' do
+      it '一覧表示画面に投稿されたものが表示されているか'do
+          expect(page).to have_content list.title
+          expect(page).to have_link list.title
+      end
+    end
+  end
+  
+  describe '詳細画面のテスト' do
+    before do
+      visit list_path(list)
+    end
+    context '表示の確認' do
+      it '削除リンクが存在しているか' do
+        expext(page).to have_link '削除'
+      end
+      it '編集リンクが存在しているか' do
+        expect(page).to have_link '編集'
+      end
+    end
+    context 'リンクの遷移先の確認' do
+      it '編集の遷移先は編集画面か' do
+        edit_link = find_all('a'[3])
+        edit_link.click
+        expect(current_path).to eq('/lists/' + list.id.to_s + '/edit')
+      end
+    end
+    context 'list削除のテスト' do
+      it 'listの削除' do
+         expect{ list.destroy }.to change{ List.count }.
+      end
+    end
+  end
 
-  describe：詳細画面のテスト
-    before：詳細画面への遷移
-    context：表示の確認
-      it：削除リンクが存在しているか
-        テストコード
-      it：編集リンクが存在しているか
-        テストコード
-    context：リンクの遷移先の確認
-      it：編集の遷移先は編集画面か
-        テストコード
-    context：list削除のテスト
-      it：listの削除
-        テストコード 
-        
-  describe：編集画面のテスト
-    before：編集画面への遷移
-    context：表示の確認
-      it：編集前のタイトルと本文がフォームに表示(セット)されている
-        テストコード
-      it：保存ボタンが表示される
-    context：更新処理に関するテスト
-      it：更新後のリダイレクト先は正しいか
-        テストコード
+  describe '編集画面のテスト' do
+    before do
+      visit edit_path
+    end
+    context '表示の確認' do
+      it '編集前のタイトルと本文がフォームに表示(セット)されている' do
+        expect(page).to have_field 'list[title]',with: list.title
+        expect(page).to have_field 'list[body]',with: list.body
+      end
+      it '保存ボタンが表示される' do
+        expect(page).to have_buttom '保存'
+      end
+    end
+    context '更新処理に関するテスト' do
+      it '更新後のリダイレクト先は正しいか' do
+        fill_in 'list[title]', with: Faker::Lorem.characters(number:10)
+        fill_in 'list[body]', with: Faker::Lorem.characters(number:30)
+        click_buttom '保存'
+        expect(page).to have_current_path list_path(list)
+      end
+    end
+  end
+end
 
-
-
-
-
-
-  config.include FactoryBot::Syntax::Methods
 end
